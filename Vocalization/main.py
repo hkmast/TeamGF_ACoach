7fdc1a2f6b4373d53bce866e048064b5fee99f4e
hkmast <kms970910@naver.com>
2022-10-21 18:40:16 +0900

    테스트 코드 정리

---
+++ Vocalization/main.py
@@ -0,0 +1,11 @@
+import warnings
+warnings.filterwarnings(action='ignore')
+
+import numpy as np
+import matplotlib.pyplot as plt
+import IPython.display as ipd
+
+import librosa
+import librosa.display
+
+file_path = 'disco.00054.wav'
