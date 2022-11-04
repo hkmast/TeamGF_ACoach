454da920c05e5cb7e93a204a31955af5507a1884
JamesKim <rudghxorb@gmail.com>
2022-11-04 15:41:02 +0900

    테스트

--- Pronunction/wav.py
+++ Pronunction/wav.py
@@ -1,6 +1,6 @@
 import wave
 
-with open("C:/Users/KimTaeGyuPC/OneDrive/바탕 화면/test_data/woman1_태규.raw", "rb") as inp_f:
+with open("C:/Users/KimTaeGyuPC/Desktop/test_data/woman1_태규.raw", "rb") as inp_f:
     data = inp_f.read()
     with wave.open("woman1_태규.wav", "wb") as out_f:
         out_f.setnchannels(1)
